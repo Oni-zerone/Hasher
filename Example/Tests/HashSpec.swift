c300573072b281343f0e8a237ddd8fe6315e3b6c
Andrea Altea <andrea.altea@ynap.com>
2019-03-13 12:11:01 +0100

    Feat: add tests for SHA1 on Hex notation.

--- Example/Tests/HashSpec.swift
+++ Example/Tests/HashSpec.swift
@@ -57,6 +57,48 @@ class HashSpec: QuickSpec {
                     expect("".hasher.md5(.base64)).to(equal("1B2M2Y8AsgTpgAmY7PhCfg=="))
                 }
             }
+            
+            context("check SHA1 hashes from strings") {
+                it("Get correct hash as HEX") {
+                    expect("Hello friend. Hello friend? That's lame. Maybe I should give you a name.".hasher.sha1())
+                        .to(equal("eefecb14f8e213a96772534c76bcc4d07d7ebff2"))
+                    expect("The douche I was dating proposed to me, can you believe? I mean, what an asshole.".hasher.sha1())
+                        .to(equal("1bfdbdb968bbf19f97153eb8a083f757ce28a78e"))
+                    expect("That neat little ditty was Trenton's idea. She may look innocent, but I'd be careful. She's got some Allah Akbar in her.".hasher.sha1())
+                        .to(equal("4edcc94527aa0553005bcda3ab7404e98f0159b0"))
+                    expect("The world is a dangerous place, Elliot, not because of those who do evil, but because of those who look on and do nothing.".hasher.sha1())
+                        .to(equal("8a0f740646877faa9dfb57ddea5ecda0f529b0c4"))
+                    expect("People who get violent get that way because they can't communicate.".hasher.sha1())
+                        .to(equal("e0d93b729ecaa45ae1ba4eaf309157b9f9bffb8e"))
+                    expect("Shit. I'm gonna have to let him hug me, aren't I?".hasher.sha1())
+                        .to(equal("d083bc7df97d805d9a1897285a79e1e8b6c054c2"))
+                    expect("True courage is about being honest with yourself. Especially when it's difficult.".hasher.sha1())
+                        .to(equal("7812af01c9df4ea0002ce34dda6756fa5820306c"))
+                    expect("Life is so much easier when you're numb.".hasher.sha1())
+                        .to(equal("b1e061b9af53918fb9318981275d2c4930702725"))
+                    expect("".hasher.sha1()).to(equal("da39a3ee5e6b4b0d3255bfef95601890afd80709"))
+                }
+                
+                xit("Get correct hash as base64 encoded") {
+                    expect("Hello friend. Hello friend? That's lame. Maybe I should give you a name.".hasher.sha1(.base64))
+                        .to(equal("zeNxEbro6N5sNgyUc5t08w=="))
+                    expect("The douche I was dating proposed to me, can you believe? I mean, what an asshole.".hasher.sha1(.base64))
+                        .to(equal("3CCsY2RWDN8AzeNDyasHDA=="))
+                    expect("That neat little ditty was Trenton's idea. She may look innocent, but I'd be careful. She's got some Allah Akbar in her.".hasher.sha1(.base64))
+                        .to(equal("eL7a0Fn/faX6dPd5/OaoFw=="))
+                    expect("The world is a dangerous place, Elliot, not because of those who do evil, but because of those who look on and do nothing.".hasher.sha1(.base64))
+                        .to(equal("6DlkaLQ6ePHbu3I30xK2cQ=="))
+                    expect("People who get violent get that way because they can't communicate.".hasher.sha1(.base64))
+                        .to(equal("IXUAgehoZNMogRObFa+2mg=="))
+                    expect("Shit. I'm gonna have to let him hug me, aren't I?".hasher.sha1(.base64))
+                        .to(equal("Wkc//jM3W8r41pw2lGIAvQ=="))
+                    expect("True courage is about being honest with yourself. Especially when it's difficult.".hasher.sha1(.base64))
+                        .to(equal("yKgWbJOFoxdeo2PBHftPUQ=="))
+                    expect("Life is so much easier when you're numb.".hasher.sha1(.base64))
+                        .to(equal("RBY9V0Rg7HFxqGCt8fiokA=="))
+                    expect("".hasher.sha1(.base64)).to(equal("1B2M2Y8AsgTpgAmY7PhCfg=="))
+                }
+            }
         }
     }
 }
